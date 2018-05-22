0812eec19d3afb441810759a331a94a36e23c460
wangyuan1 <wangyuan1@corp.netease.com>
2018-05-22 17:43:30 +0800

    初始

---
+++ src/main/java/com/wy/ForeachTest.java
@@ -0,0 +1,9 @@
+package com.wy;
+
+/**
+ * @Auther: wangyuan1
+ * @Date: 2018/5/22 17:38
+ * @Description:
+ */
+public class ForeachTest {
+}
---
+++ src/main/java/com/wy/User.java
@@ -0,0 +1,20 @@
+package com.wy;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+/**
+ * @Auther: wangyuan1
+ * @Date: 2018/5/22 17:38
+ * @Description:
+ */
+@Getter
+@Setter
+public class User {
+    private Integer id;
+    private String name;
+
+    public Boolean isStudent() {
+        return id % 2 == 0;
+    }
+}
